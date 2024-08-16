d0374f7d64a4d80d76e69e0bdb1be10e1686bb87
martinyelland <martin.yelland@rejuvenate.it>
2024-08-16 11:47:33 +0100

    Create Install-ChocoApps.ps1

---
+++ Install-ChocoApps.ps1
@@ -0,0 +1,25 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
+# Check Permissions
+if ( -Not( (New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) ){
+    Write-Error -Message "Script needs Administrator permissions"
+    exit 1
+}
+
+if (-Not (Get-Command "choco" -errorAction SilentlyContinue)) {
+   [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+   iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+
+choco feature enable -n=allowGlobalConfirmation
+choco install 7zip
+choco install vlc
+choco install paint.net
+choco install inkscape
+choco install notepadplusplus
+choco install notepadreplacer --params "'/NOTEPAD:C:\Program Files\Notepad++\notepad++.exe'"
+choco install cutepdf
+choco install sumatrapdf.install
+choco install greenshot
+choco install powerbi
+# choco install keeper
