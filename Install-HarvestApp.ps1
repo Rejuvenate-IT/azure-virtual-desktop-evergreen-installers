c2510d603ec711b68b03b26c9a423da3e8ec40b7
martinyelland <martin.yelland@rejuvenate.it>
2024-08-21 16:03:35 +0100

    Update Install-HarvestApp.ps1

--- Install-HarvestApp.ps1
+++ Install-HarvestApp.ps1
@@ -58,8 +58,11 @@ $appxFiles = Get-ChildItem -Path $folderPath -Filter "*.appx"
 foreach ($appxFile in $appxFiles) {
     # Run the desired command for each file
     # Replace "x" with the desired command
-    $msxifilepath = $msixFile.FullName
+    $appxFilePath = $appxFile.FullName
+
+    # Provision the Appx package
+    write-host $appxFilePath
+    DISM.EXE /Online /Add-ProvisionedAppxPackage /PackagePath:$appxFilePath /SkipLicense /Region:"all"
 }
 
-# Provision the Appx package
-DISM.EXE /Online /Add-ProvisionedAppxPackage /PackagePath:$msxifilepath /SkipLicense /Region:"all"
+
