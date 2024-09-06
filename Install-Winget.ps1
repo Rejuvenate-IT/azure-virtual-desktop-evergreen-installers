dd834035c53312bab1478056bdb6ed8cbc0af34e
martinyelland <martin.yelland@rejuvenate.it>
2024-09-06 15:20:48 +0100

    Update Install-Winget.ps1

--- Install-Winget.ps1
+++ Install-Winget.ps1
@@ -1,6 +1,6 @@
 # get latest download url
 $URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json | Select-Object -ExpandProperty "assets" | Where-Object "browser_download_url" -Match '.msixbundle' | Select-Object -ExpandProperty "browser_download_url"
+$URL = (Invoke-WebRequest -UseBasicParsing -Uri $URL).Content | ConvertFrom-Json | Select-Object -ExpandProperty "assets" | Where-Object "browser_download_url" -Match '.msixbundle' | Select-Object -ExpandProperty "browser_download_url"
 
 # download
 Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
